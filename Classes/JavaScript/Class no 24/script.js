79120dcd7725209cab4102d129553d8af623bf4b
MalikHira296 <malikhira296ak@gmail.com>
2024-11-20 15:58:58 +0500

    For in and For of Loops

--- Classes/JavaScript/Class no 24/script.js
+++ Classes/JavaScript/Class no 24/script.js
@@ -9,23 +9,82 @@
 // var listOfLaptops = new Laptops("dell", "green",true,16)
 // console.log(listOfLaptops)
 
+//For In Loop
 
-
-var listOfLaptops = {
-    company: "dell",
-    color: "green",
-    RAM: "16GB"
-}
-
-for(let item in listOfLaptops){
-console.log(item)
-}
-
-
-
-// for(i=0;i < Object.keys(listOfLaptops).length;i++){
-//     console.log(listOfLaptops[keys])
+// var listOfLaptops = {
+//     company: "dell",
+//     color: "green",
+//     RAM: "16GB"
 // }
+
+// for(let item in listOfLaptops){
+// console.log(item,listOfLaptops[item])
+// }
+
+//For in Loop on Arrays
+
+// var arrayOfStudents = ["Student1", "Student2", "Student3","Student4","Student5"]
+
+// for(abc in arrayOfStudents){
+//     console.log(abc,arrayOfStudents[abc])
+// }
+
+
+
+
+
+
+
+//For of Loop on Arrays
+
+// const fruits = ["apple", "banana", "cherry"];
+
+// for (const value of fruits) {
+//   console.log(value);
+// }
+
+
+
+
+
+//For of Loop on Object
+
+// const person = {
+//      name: "Alice", 
+//      age: 25,
+//      city: "New York",
+//      country: "USA",
+//      hobbies: ["reading", "painting", "cooking"]
+// };
+
+// for(let values in person){
+//     console.log(values);
+    
+// }
+
+
+// for(let values of Object.keys(person)){
+//     console.log(values, person[values]);
+    
+// }
+
+
+
+
+
+
+var countryName = "Pakistan"
+
+for(let abc of countryName){
+    console.log(abc)
+}
+
+
+for(let abc in countryName){
+    console.log(abc)
+}
+
+
 
 
 
