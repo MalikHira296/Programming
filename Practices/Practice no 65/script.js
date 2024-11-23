6b87ed9f296caeb42d96342afbeba9fb961cb637
MalikHira296 <malikhira296ak@gmail.com>
2024-11-23 15:49:21 +0500

    Array Loops

--- Practices/Practice no 65/script.js
+++ Practices/Practice no 65/script.js
@@ -1,35 +1,35 @@
-// For in Loop on object and array
+// // For in Loop on object and array
 
-var  abc = {
-  name: 'John',
-  age: 30,
-  city: 'New York'
-};
+// var  abc = {
+//   name: 'John',
+//   age: 30,
+//   city: 'New York'
+// };
 
-for(let wer in abc){
-    console.log(wer,abc[wer]);
-}
+// for(let wer in abc){
+//     console.log(wer,abc[wer]);
+// }
 
-var abc = ["student1","student2","student3","student4","student5"]
+// var abc = ["student1","student2","student3","student4","student5"]
 
-for(let wer in abc){
-    console.log(wer,abc[wer]);
-}
+// for(let wer in abc){
+//     console.log(wer,abc[wer]);
+// }
 
-// For of Loop on array and object
+// // For of Loop on array and object
 
-var  abc = {
-  name: 'John',
-  age: 30,
-  city: 'New York'
-};
+// var  abc = {
+//   name: 'John',
+//   age: 30,
+//   city: 'New York'
+// };
 
-for(let rew of Object.keys(abc)){
-    console.log(rew,abc[rew]);
-}
+// for(let rew of Object.keys(abc)){
+//     console.log(rew,abc[rew]);
+// }
 
-var wer = ["student1","student2","student3","student4","student5"]
+// var wer = ["student1","student2","student3","student4","student5"]
 
-for(let rew of wer){
-    console.log(rew,wer[rew]);
-}
+// for(let rew of wer){
+//     console.log(rew,wer[rew]);
+// }
---
+++ Practices/Practice no 66/script.js
@@ -0,0 +1,41 @@
+// var rew = ["student1","student2","student3","student4","student5"]
+
+// for(let abc in rew){
+//     console.log(abc,rew[abc])
+// }
+
+// for(let abc of rew){
+//     console.log(abc,abc[rew])
+// }
+
+// var Rew = {
+//     company : "dell",
+//     screenSize : 16,
+//     touchScreen :  true
+// }
+
+// for(let abc in Rew){
+//     console.log(abc,Rew[abc])
+// }
+
+// for(let abc of Object.keys(Rew)){
+//     console.log(abc,Rew[abc])
+// }
+
+// var rew = "Pakistan"
+
+// for(let abc of rew){
+//     console.log(abc)
+// }
+
+// var rew = ["student1","student2","student3","student4","student5"]
+
+
+
+// function wer(element, index){
+//     console.log(element, index)
+//     return element
+// }
+
+// var result = rew.forEach(wer)
+// console.log("Result =>",result)
