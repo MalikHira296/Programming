eb6df76aa21c49d4b6c834db6b9e064d65068ea1
MalikHira296 <malikhira296ak@gmail.com>
2024-11-23 16:21:27 +0500

    array methods

---
+++ Classes/JavaScript/Class no 25/script.js
@@ -0,0 +1,47 @@
+// var arr = [10 , 20 ,30 ,40 ,50]
+// // var arr = [20, 20 ,20 ,20 ,20]
+
+
+// arr.reverse()
+
+// console.log(arr);
+
+
+
+
+// function arrFun(item, index) {
+//     return item == 20;
+// }
+
+// var result = arr.some(arrFun)
+// console.log(result);
+
+
+// var result = arr.every(arrFun)
+// console.log(result);
+
+// var result = arr.findIndex(arrFun)
+// console.log(result);
+// var result = arr.find(arrFun)
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
