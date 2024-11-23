236e838c1302b5427601ef35bff19e26749c1710
MalikHira296 <malikhira296ak@gmail.com>
2024-11-23 15:47:26 +0500

    revise

---
+++ Practices/Practice no 67/script.js
@@ -0,0 +1,56 @@
+var rew = ["student1","student2","student3","student4","student5"]
+
+// function wer(element,index,arr) {
+//     console.log(element,index,arr);    
+//      return 
+// }
+
+
+
+
+var poi = rew.map((element,index) =>{return element})
+console.log("return value =>",poi);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
