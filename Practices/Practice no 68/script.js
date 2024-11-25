bb8a0599996173dacd8fce6052e09c9c30a0711c
MalikHira296 <malikhira296ak@gmail.com>
2024-11-25 15:36:59 +0500

    practice

---
+++ Practices/Practice no 68/script.js
@@ -0,0 +1,29 @@
+// var wer = [10 , 20 ,30 ,40 ,50]
+// var wer = [20, 20 ,20 ,20 ,20]
+
+
+// wer.reverse()
+
+// console.log(wer);
+
+
+
+
+// function werFun(item, index) {
+//     return item == 20;
+// }
+
+// var valueResult = wer.some(werFun)
+// console.log(valueResult);
+
+
+// var valueResult = wer.every(werFun)
+// console.log(valueResult);
+
+// var valueResult = wer.findIndex(werFun)
+// console.log(valueResult);
+
+// var valueResult = wer.find(werFun)
+// console.log(valueResult);
+
+
---
+++ Practices/Practice no 69/script.js
@@ -0,0 +1,50 @@
+// var rew = [20,30,45,90,110]
+
+// function wer(elements,indexs){
+//     console.log(elements,indexs)
+// }
+// rew.some(wer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
