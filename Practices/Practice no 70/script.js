da173e62f750b3764e316349bb3ef7cce18ea59e
MalikHira296 <malikhira296ak@gmail.com>
2024-11-26 15:37:56 +0500

    revise

--- Practices/Practice no 70/script.js
+++ Practices/Practice no 70/script.js
@@ -1,1 +1,22 @@
-g
+// var wer = [46,30,40,50,90]
+
+// var wre = wer.reverse()
+// console.log(wre);
+
+// var wer = [20,20,20,20,20]
+// function arrFunc(item,index) {
+//     return item > 45;
+// }
+
+// var wre = wer.some(arrFunc)
+// console.log(wre);
+
+// var wre = wer.every(arrFunc)
+// console.log(wre);
+
+// var wre = wer.find(arrFunc)
+// console.log(wre);
+
+// var wre = wer.findIndex(arrFunc)
+// console.log(wre);
+
