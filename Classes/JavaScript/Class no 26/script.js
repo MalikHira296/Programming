de35cb04d659b2436683e3f0457398abe1cb9dd5
MalikHira296 <malikhira296ak@gmail.com>
2024-11-26 17:13:49 +0500

    array methods

---
+++ Classes/JavaScript/Class no 26/script.js
@@ -0,0 +1,64 @@
+// var arr = new Array()
+
+// for (var i = 0; i < 10; i++) {
+
+//   arr[i] = i * 2;
+
+// }
+
+// console.log(arr);
+
+
+
+// var num = [10, 20, 30, 40, 50];
+
+// var result = num.indexOf(80)
+
+// console.log(result);
+
+
+
+
+
+
+
+// var arr1 = [10,20,30,40,50]
+
+// var arr2 = [5,15,25]
+
+
+// var result = arr2.concat(arr1)
+
+// console.log("Result =>>",result);
+
+// console.log("Arr2 =>>",arr2);
+// console.log("Arr1 =>>",arr1);
+
+// var arr1 = [30,50,10,20,90]
+
+// var result = arr1.sort((a,b)=>b - a)
+// var result = arr1.sort((a,b)=>{
+//     var c = b - a
+//     return c
+// })
+// function abc(a , b) {
+//     // console.log("a = ",a,"b = " ,b);
+//     return a - b;
+// }
+
+
+// var result = arr1.sort(abc)
+// console.log("Result =>>",result);
+
+
+
+// var arr1 = [30,50,10,20,90]
+
+// var result = arr1.includes(90)
+
+// console.log("Result =>>",result);
+
+
+
+
+
---
+++ Practices/Practice no 71/script.js
@@ -0,0 +1,121 @@
+// var arr = [10,20,30,40,50]
+
+// var wer = arr.indexOf(45)
+// console.log(wer)
+
+// var arr1 = [20,40,50,30,10]
+// var arr2 = [15,25,35,45,55]
+
+// var wer = arr1.concat(arr2)
+
+// console.log(wer)
+
+// var arr = [20,40,50,30,10]
+
+// // function rew (a,b){
+// //     return b - a
+// // }
+// var wer = arr.sort((a,b)=>a - b)
+
+// console.log(wer)
+
+
+
+// var arr = [20,30,40,50]
+
+// var wer = arr.includes(30)
+
+// console.log(wer)
+
+
+
+// ------------------------------------------------------------------------------------//
+
+
+// var arr = [10,20,30,40,50]
+
+// var wer = arr.reverse(arr)
+
+// console.log(wer)
+
+
+// var arr = [10,20,30,40,50]
+
+// function wre(item,index){
+//     return item == 20
+// }
+
+// var wer = arr.some(wre)
+// console.log(wer)
+
+
+
+// var wer = arr.every(wre)
+// console.log(wer)
+
+
+
+// var wer = arr.find(wre)
+// console.log(wer)
+
+// var wer = arr.findIndex(wre)
+// console.log(wer)
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+// var arr = ["wer1","wer2","wer3","wer4","wer5"]
+
+// arr.push("wer90")
+
+// arr.pop()
+
+// arr.shift()
+
+// arr.unshift("wer")
+
+// console.log(arr)
+
+
+
+// var arr = ["wer1","wer2","wer3","wer4","wer5"]
+
+// function wre(el,index){
+//    console.log(el,index)
+//    return
+// }
+
+// arr.forEach(wre)
+// var wer = arr.forEach(wre)
+// console.log(wer)
+// arr.map(wre)
+// var wer = arr.map(wre)
+// console.log(wer)
+// arr.filter(wre)
+// var wer = arr.filter(wre)
+// console.log(wer)
+
+
+
+// var arr = ["wer1","wer2","wer3","wer4","wer5"]
+
+// // arr.splice(0,3,"wrewr")
+
+
+// var result = arr.slice(0 , 5)
+
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
