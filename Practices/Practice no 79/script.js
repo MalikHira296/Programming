c9b4ce330f03ed8dbe59f6cc899b9135676f7d4c
MalikHira296 <malikhira296ak@gmail.com>
2024-12-08 21:13:23 +0500

    work is done

--- Practices/Practice no 79/script.js
+++ Practices/Practice no 79/script.js
@@ -1,7 +1,57 @@
-function wer() {
-    var tree = document.getElementById('hello')
-    var tree = document.getElementById('')
-}
+
+
+var obj = new Date();
+
+var allDate = obj.getDate()
+// var allDate = obj.getDay()
+
+// var allDate = obj.getFullYear()
+
+// var allDate = obj.getHours()
+
+// var allDate = obj.getMinutes()
+
+// var allDate = obj.getSeconds()
+
+// var allDate = obj.getMilliseconds()
+
+// var allDate = obj.getTime()
+
+// var allDate = obj.getUTCDate()
+
+// var allDate = obj.getUTCDay()
+
+// var allDate = obj.getUTCFullYear()
+
+// var allDate = obj.getUTCHours()
+
+console.log(allDate);
+
+// console.log(obj.getFullYear);
+
+// var obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// JSON.stringify(obj)
+// JSON.parse(obj)
+
+// console.log("First");
+
+// async function onCick() {
+//     console.log("Hello World");
+// }
+
+// try {
+//     onCLick()
+// } catch (error) {
+//     console.log("Error=> ", error);
+// }
+
+
+// console.log("Hello @nd");
+
 
 
 
---
+++ Practices/Practice no 80/script.js
@@ -0,0 +1,54 @@
+// function tree() {
+//     var rew = document.getElementById('hello')
+//     rew.style.backgroundColor = 'yellow'    
+//     console.log(rew);
+    
+// }
+
+
+function tree() {
+    var wer = document.getElementById('hello') 
+    // var wer2 = document.getElementById('button')
+    // wer2.className = 'btn'  
+    wer.style.backgroundColor = 'yellow'
+    // wer.style.fontSize = '200px'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
